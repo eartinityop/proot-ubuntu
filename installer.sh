b05dc5560aa3875fd4359f8fdab60ca1c06a8422
Eartinity <77088825+eartinityop@users.noreply.github.com>
2024-03-10 07:26:34 +0530

    installer: Initial commit

---
+++ installer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Install proot 
+apt download proot
+mkdir ~/proot
+mkdir ~/bin
+dpkg -x proot*.deb ~/proot
+cd proot/usr/bin
+mv proot ~/bin
+export PATH="$HOME/bin:$PATH"
+
+# Detect the machine architecture
+ARCH=$(uname -m)
+
+if [ "$ARCH" = "x86_64" ]; then
+  ARCH_ALT=amd64
+elif [ "$ARCH" = "aarch64" ]; then
+  ARCH_ALT=arm64
+else
+  printf "Unsupported CPU architecture: ${ARCH}"
+  exit 1
+fi
+
+# Fetch Ubuntu rootfs image as per the machine architecture and install Ubuntu 
+cd 
+mkdir ubuntu
+wget https://github.com/termux/proot-distro/releases/download/v4.8.0/ubuntu-focal-$ARCH_ALT-pd-v4.8.0.tar.xz
+tar -xf ubuntu*.xz -C ubuntu
+proot -r ubuntu/ubuntu* -O /bin/bash
